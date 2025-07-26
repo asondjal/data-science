89b73134b5c736b36e55076ef1d9d2ab0e304334
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-07-26 18:51:33 +0200

    Minor changes

--- cpp_tests/main.cpp
+++ cpp_tests/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 #include "tests/test_read_data.hpp"
+#include "tests/test_statistik.hpp"
+#include "tests/test_logging.hpp"
 
 int main() {
   TestReadData();
+  TestStatistik();
+  TestLogging();
 
   return 0;
 }
