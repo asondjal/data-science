ca7d2b353d422687313b50010c554c812ba1d459
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-09-06 22:31:26 +0200

    Minor changes due to formatting

--- python_tests/test_logging.py
+++ python_tests/test_logging.py
@@ -1,6 +1,7 @@
 """Python-file for testing class Logging"""
 
-from py_bindings import Logging, LogLevel
+from intersection import Logging, LogLevel
+
 
 def test_logging():
     """Funktion zum Testen von LogLevel"""
@@ -18,5 +19,6 @@ def test_logging():
     logger.start_log("FBI has hacked your PC!", LogLevel.WARNING)
     logger.start_log("WIFI-Connection was interrupted!", LogLevel.ERROR)
     logger.start_log("WIFI-Connection was interrupted by HTTP-Port 241", LogLevel.DEBUG)
+
 
 test_logging()
