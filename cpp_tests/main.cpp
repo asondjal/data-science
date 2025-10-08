532e9174f6040907c6a07fb07809f9f8b9f66b26
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-10-08 23:27:38 +0200

    Minor changes due to style modifications

--- cpp_tests/main.cpp
+++ cpp_tests/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-#include "tests/test_arithmetic.hpp"
 #include "tests/test_logging.hpp"
 #include "tests/test_read_data.hpp"
 #include "tests/test_statistik.hpp"
@@ -9,7 +8,6 @@ int main() {
   TestReadData();
   TestStatistik();
   TestLogging();
-  TestArithmetic();
 
   return 0;
 }
